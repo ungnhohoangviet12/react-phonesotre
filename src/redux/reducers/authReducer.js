3362790e91aee1dd423c91e8ce51d5ceb18125df
Ung Nho Hoang Viet <ungnhohoangviet1@gmail.com>
2022-08-04 18:04:25 +0700

    add data

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -26,29 +26,11 @@ export const authReducer = (state = initialState, action) => {
             } else !!navigate && navigate('/')
             return { ...state };
         }
-        // case types.GET_PROFILE_SUCCESS: {
-        //     const profile = action.payload.profile;
-        //     state = {
-        //         ...state,
-        //         profile: profile,
-        //         isLoggIn: true,
-        //         isAuthenticated: profile.role,
-        //     };
-        //     return { ...state };
-        // }
         case types.LOGOUT: {
             localStorage.removeItem(INFO_USER_KEY)
             return { ...initialState };
         }
-        // case types.GET_PROFILE_FAIL: {
-        //     return { ...initialState };
-        // }
-        // case types.LOGIN_FAIL: {
-        //     return { ...state, isLoading: false, notif: 'loginFail' };
-        // }
-        // case types.SET_IS_LOADING: {
-        //     return { ...state, isLoading: true };
-        // }
+
         default:
             return state;
     }
